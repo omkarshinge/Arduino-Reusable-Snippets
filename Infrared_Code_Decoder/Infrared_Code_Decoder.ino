95f4ef3fba0353cd330d1b1039c190150ee7ea97
Omkar Shinge <oshinge@gmail.com>
2020-05-02 15:01:14 +0530

    Add code to read IR code

--- Infrared_Code_Decoder/Infrared_Code_Decoder.ino
+++ Infrared_Code_Decoder/Infrared_Code_Decoder.ino
@@ -1,7 +1,15 @@
+#include <IRremote.h>
+int receiver_pin = 8;
+IRrecv receiver(receiver_pin);
+decode_results results;
 void setup() {
-
+  Serial.begin(9600);
+  receiver.enableIRIn();
 }
 
 void loop() {
-
+  if(receiver.decode(&results)){
+    Serial.println(results.value, HEX);
+    receiver.resume();
+  }
 }
